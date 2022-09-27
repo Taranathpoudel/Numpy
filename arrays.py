24a8741d060c8941931d03de8df8d90d97635cc4
Tara Nath Poudel <poudeltaranath@gmail.com>
2022-09-27 17:38:06 +0545

    type of array

---
+++ arrays.py
@@ -0,0 +1,16 @@
+# Thera are 0,1,2.....n dimensional arrays
+# zero dimensional array
+from re import M
+import numpy as np
+arr=np.array(50)
+print(arr)
+
+
+
+# one dimensional arrays
+t=np.array([1,2,3])
+print(t)
+
+# Tqo or multidimensional array
+m=np.array([[1,2,3,4,5],[1,5,9,3,5,7]])
+print(m)
