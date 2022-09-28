bf2c3ecc100c2ea020230553eba7e5b37842246e
Tara Nath Poudel <poudeltaranath@gmail.com>
2022-09-28 21:15:16 +0545

    search

--- Searching_in_array.py
+++ Searching_in_array.py
@@ -2,7 +2,7 @@ import numpy as np
 # Use where() with numpy to get the element searched
 # It returns the index number of the element of the array
 a=np.array([1,2,3,2,5,2,7,2,2])
-x=np.where(a==2)
+x=np.where(a==2) # condition can be applied to the array for search
 print(x)
 for i in x:
     print(a[i])
